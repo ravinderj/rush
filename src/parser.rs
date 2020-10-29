a13192fcfd046d6708ee1cfeb74874e0d6be55ef
ravinderj <jajoriaravinder99@gmail.com>
2020-10-29 09:46:32 +0530

    Basic lexing

--- src/parser.rs
+++ src/parser.rs
@@ -1,7 +1,7 @@
 use nom::{
   branch::alt,
   bytes::complete::{escaped, take_while, take_while1},
-  character::complete::{alphanumeric1 as alphanumeric, char, one_of},
+  character::complete::{alphanumeric1 as alphanumeric, char, one_of, space1},
   combinator::{cut, map},
   error::{context, ErrorKind, ParseError},
   IResult,
@@ -13,6 +13,7 @@ pub enum Token<'a> {
   Var(&'a str),
   Literal(&'a str),
   Substitution(&'a str, Vec<Token<'a>>),
+  Word(&'a str), // Need to think of some other name
   Pipe,
 }
 
@@ -40,6 +41,18 @@ fn var<'a, E: ParseError<&'a str>>(
   )(i)
 }
 
+fn word<'a, E: ParseError<&'a str>>(
+  i: &'a str,
+) -> IResult<&'a str, Token<'a>, E> {
+  context(
+    "word",
+    map(
+      take_while1(is_valid_var_char),
+      Token::Word,
+    ),
+  )(i)
+}
+
 fn literal<'a, E: ParseError<&'a str>>(
   i: &'a str,
 ) -> IResult<&'a str, Token<'a>, E> {
@@ -49,7 +62,7 @@ fn literal<'a, E: ParseError<&'a str>>(
 fn parse_str<'a, E: ParseError<&'a str>>(
   i: &'a str,
 ) -> IResult<&'a str, &'a str, E> {
-  escaped(alphanumeric, '\\', one_of("\"n\\"))(i)
+  escaped(alt((alphanumeric, space1)), '\\', one_of("\"n\\"))(i)
 }
 
 fn string<'a, E: ParseError<&'a str>>(
@@ -70,7 +83,7 @@ fn pipe<'a, E: ParseError<&'a str>>(
 fn segment<'a, E: ParseError<&'a str>>(
   i: &'a str,
 ) -> IResult<&'a str, Token<'a>, E> {
-  context("segment", preceded(sp, alt((literal, pipe, var))))(i)
+  context("segment", preceded(sp, alt((word, literal, pipe, var))))(i) // this seems wrong
 }
 
 fn root<'a, E: ParseError<&'a str>>(
@@ -79,10 +92,26 @@ fn root<'a, E: ParseError<&'a str>>(
   map(segment, |tuple_vec| vec![tuple_vec])(i)
 }
 
-pub fn parse(i: &str) -> Vec<Token> {
-  let result = root::<(&str, ErrorKind)>(i);
-  println!("{:#?}", result);
-  vec![]
+pub fn parse(i: &str) -> Vec<Token> { //this seems wrong
+  let mut all_tokens : Vec<Token> = Vec::new();
+  let mut new_i = i.clone();
+  loop {
+    match root::<(&str, ErrorKind)>(new_i) {
+      Ok((remaining, tokens)) => {
+        all_tokens.extend(tokens);
+        println!("{:?}", remaining);
+        if remaining.len() == 0 {
+          break
+        }
+        new_i = remaining;
+      }
+      Err(e) => {
+        println!("{:?}", e);
+        break
+      }
+    }
+  }
+  all_tokens
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -93,6 +122,8 @@ pub fn parse(i: &str) -> Vec<Token> {
 pub mod tests {
   use crate::parser::{parse, Token};
   use crate::parser::Token::Literal;
+  use crate::parser::Token::Pipe;
+  use crate::parser::Token::Word;
 
   #[test]
   fn should_parse_empty_string() {
@@ -104,16 +135,30 @@ pub mod tests {
   #[test]
   fn should_parse_multiple_words() {
     let actual = parse(r#""hello""#);
-    let expected: Vec<Token> = vec![Literal("hello world")];
+    let expected: Vec<Token> = vec![Literal("hello")];
     assert_eq!(actual, expected)
   }
-  //
-  //  #[test]
-  //  fn should_parse_word_with_string() {
-  //    let actual = parse("echo \"hello world\"".to_owned());
-  //    let expected: Vec<Token> = vec![Value(String::from("echo")), Values(vec![String::from("hello world")])];
-  //    assert_eq!(expected, actual)
-  //  }
+
+  #[test]
+  fn should_parse_pipe() {
+    let actual = parse(r#""hello" | "#);
+    let expected: Vec<Token> = vec![Literal("hello"), Pipe];
+    assert_eq!(actual, expected)
+  }
+
+   #[test]
+   fn should_parse_word_with_string() {
+     let actual = parse(r#"echo "hello world""#);
+     let expected: Vec<Token> = vec![Word("echo"), Literal("hello world")];
+     assert_eq!(expected, actual)
+   }
+
+   #[test]
+   fn should_parse_word_with_string_and_pipe() {
+     let actual = parse(r#"ls | grep "catch me if you can""#);
+     let expected: Vec<Token> = vec![Word("ls"), Pipe, Word("grep"), Literal("catch me if you can")];
+     assert_eq!(expected, actual)
+   }
   //
   //  #[test]
   //  fn should_parse_word_with_escaped_space_string() {
